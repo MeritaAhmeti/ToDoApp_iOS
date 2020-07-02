8442cf455f99a503c9b63a8daf1c63fb5b208e50
MeritaAhmeti <meritaahmeti1998@gmail.com>
2020-07-02 07:03:47 -0700

    Initial Commit

---
+++ ToDoApp_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoApp_iOS
+//
+//  Created by Merita Ahmeti on 7/2/20.
+//  Copyright © 2020 Merita Ahmeti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
